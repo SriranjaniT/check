fa16f957b0fded1a2e072902127cc9b1e03205e6
SriranjaniT <sriranjanit9@gmail.com>
2023-06-23 09:43:47 +0530

    third program

---
+++ src/pro3/Main1.java
@@ -0,0 +1,15 @@
+package pro3;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num=7;
+        int sum=0;
+        for (int i=1;i<=num;i++)
+        	sum+=i;
+        System.out.println(sum);
+        	
+	}
+
+}
